a718d6f8e8fd801343511e80f4d271e5f71daf58
mdwhitt77gngde <u585507859@byui.edu>
2024-05-18 22:10:19 -0500

    album

---
+++ scripts/Temples.js
@@ -0,0 +1,12 @@
+document.getElementById('copyright').textContent=
+'Copyright ©️' + new Date(),getFullYear() + ' - Last modified: ' + document.lastModified;
+
+const navMenu = document.querySelector('nav');
+const hamburger = document.createElement('button');
+hamburger.innerHTML = '&#9776;';
+
+document.querySelector('header').appendChild(hamburger);
+
+hamburger.addEventListener('click', function() {
+    navMenu.classList.toggle('show');
+});
